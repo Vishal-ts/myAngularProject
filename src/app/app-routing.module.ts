c171f3339afa3b3fe5c711feb99bc7a2173bc131
Vishal-ts <svishal65120@gmail.com>
2022-04-28 19:36:00 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SchoolComponent } from './Components/school/school.component';
+import { School1Component } from './school1/school1.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'school1',component:School1Component},
+{path:'school',component:SchoolComponent},  {path:'',redirectTo:'/school',pathMatch:'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SchoolComponent } from './Components/school/school.component';
+import { FormsModule } from '@angular/forms';
+import { School1Component } from './school1/school1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchoolComponent,
+    School1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
